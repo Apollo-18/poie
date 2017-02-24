48197beedb033905f0ea084e27afecee8f70dc17
Alexander <alexander@plav.in>
2017-02-24 22:53:49 +0300

    Update

--- setup.py
+++ setup.py
@@ -1,43 +1,22 @@
-from setuptools import setup. find_packages
+from setuptools import setup, find_packages
 
 setup(
     name='poie',
-
     version='0.1a',
-
     description='Poisson Image Editing',
-
-    # Author details
     author='Απόλλων',
-
-    # Choose your license
     license='MIT',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 2 - Pre-Alpha',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3.6',
     ],
-
-    # What does your project relate to?
-    keywords='poisson image editing python',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
+    keywords='poisson equation image editing seamless cloning texture flattening local illumination changes',
     packages=find_packages(),
 
     # List run-time dependencies here.  These will be installed by pip when
